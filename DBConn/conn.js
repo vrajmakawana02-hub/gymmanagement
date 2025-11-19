d8bf1cdf5db0351fbba17ebfcd32664de293c351
vrajmakawana02-hub <vrajmakawana02@gmail.com>
2025-11-20 01:39:13 +0530

    Update conn.js

--- DBConn/conn.js
+++ DBConn/conn.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://vrajmakawana02_db_user:vraj1234%2E@cluster0.qhlcqn4.mongodb.net/gymBackend?retryWrites=true&w=majority&appName=Cluster0')
-.then(() => console.log("DB connection successful"))
-.catch(err => console.log(err));
+mongoose
+  .connect("mongodb+srv://vrajmakawana02_db_user:vraj1234@cluster0.qhlcqn4.mongodb.net/gymBackend")
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
